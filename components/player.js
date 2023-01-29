06fd33be8d63c6122176eb648cbe56c5aba617e5
SugatoBagchi <sugato.bagchi.of@gmail.com>
2023-01-29 08:11:01 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ components/player.js
@@ -0,0 +1,96 @@
+import Header from './header'
+import Nav from './nav'
+import Playlist from './playlist'
+import PlayerControls from './playerControls'
+import Activity from './activity'
+import { useState, useEffect } from 'react'
+import UploadModal from './UploadModal'
+import usemusix from '../hooks/usemusix'
+import Head from 'next/head'
+
+const style = {
+    wrapper: ` bg-[#15202b] flex-[2] h-screen min-h-screen p-6 no-scrollbar overflow-y-scroll select-none h-full w-full rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-90 border border-gray-100
+`,
+    searchbox: `flex align-center justify-center w-full mb-6`,
+    searchentry: `w-full bg-gray-200 rounded-lg p-2 text-black`,
+    searchbutton: `bg-blue-500 text-white rounded-lg p-2 pl-4 ml-5`,
+};
+
+const Player = () => {
+    const [showUploadMusic, setShowUploadMusic] = useState(false)
+    const [title, setTitle] = useState('')
+    const [musicUrl, setMusicUrl] = useState('')
+    const [showDiv, setShowDiv] = useState(false);
+
+
+    const { newMusic, getSongs } = usemusix(
+        musicUrl,
+        title,
+        setTitle,
+        setMusicUrl,
+        setShowUploadMusic,
+    )
+
+    const [songs, setSongs] = useState([])
+
+    useEffect(() => {
+        getSongs().then(songs => {
+            setSongs(songs)
+        })
+    }, [])
+
+    useEffect(() => {
+        if (showDiv) {
+          // load the new division here
+        }
+    }, [showDiv]);
+    
+
+    const query = ''
+
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(searchTerm);
+    }
+
+    return (
+        <>
+        <div className={style.wrapper}>
+            <Header setShowUploadMusic={setShowUploadMusic} />
+
+            <form className={style.searchbox} onSubmit={handleSubmit}>
+                <input
+                    className={style.searchentry}
+                    type="text"
+                    placeholder="Search"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                
+                <button className={style.searchbutton} onClick={handleSubmit}>
+                    Search
+                </button>
+            </form>
+        
+            <Playlist songs={songs} searchId={searchTerm} />
+
+
+            </div>
+            <PlayerControls songs={songs} />
+            {showUploadMusic && (
+                <UploadModal
+                title={title}
+                setTitle={setTitle}
+                setShowUploadMusic={setShowUploadMusic}
+                musicUrl={musicUrl}
+                setMusicUrl={setMusicUrl}
+                newMusic={newMusic}
+                />
+            )}
+        </>
+    )
+}
+
+export default Player
